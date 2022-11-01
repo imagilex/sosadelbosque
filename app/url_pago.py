76060cf1e9fb2afea6235ae24a365ffacb3d2553
Rubén Ramírez Gómez <maths.unam@gmail.com>
2022-11-01 17:26:22 -0600

    Payments updates

--- app/url_pago.py
+++ app/url_pago.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('actualizar/<pk>/', views.pago_update, name=f"{object_name}_update"),
     path('eliminar/<pk>/', views.pago_delete, name=f"{object_name}_delete"),
     path('pagado/<pk>/', views.pago_update_status, name=f"{object_name}_pagado"),
+    path('pago-cte/<pk>/', views.pago_update_status_cte, name=f"{object_name}_pagado_cte"),
     ]
